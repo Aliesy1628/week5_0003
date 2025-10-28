5c52aceeb3229fc914056a587b9ad163e221e935
Aliesy1628 <gulaku1628@gmail.com>
2025-10-28 18:26:40 +0700

    Latihan/testinf

---
+++ testing.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class testing {
+    public static void main(String[] args) {
+
+        Scanner ns = new Scanner(System.in);
+        int hargaBlender;
+        double totalHarga;
+        String status;
+
+        System.out.print("Masukan harga blender : ");
+        hargaBlender = ns.nextInt();
+        ns.nextLine();
+
+        System.out.print("Masukan status member : ");
+        status = ns.nextLine();
+
+        if (status.contentEquals("ya") && hargaBlender > 500000) {
+            System.out.println("Mendapat diskon 10%");
+            totalHarga = hargaBlender - (hargaBlender * 0.10);
+            System.out.println("Total harga : " + totalHarga);
+        } else {
+            if (status.contentEquals("tidak") && hargaBlender > 1000000) {
+                System.out.println("Mendapat diskon 5%");
+                totalHarga = hargaBlender - (hargaBlender * 0.5);
+                System.out.println("Total harga : " + totalHarga);
+            }
+        }
+        ns.close();
+    }
+}
